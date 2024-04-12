b7cd9f18d6f336114c8f87ed774324949f0097aa
Giorgi Bliadze <bliadze1997@gmail.com>
2024-04-12 15:37:06 +0400

    few changes in size

--- components/Circles.js
+++ components/Circles.js
@@ -3,7 +3,7 @@ import Image from "next/image";
 
 const Circles = () => {
   return (
-    <div className="w-[200px] xl:w-[300px] absolute -right-16 -bottom-[120px] mix-blend-color-dodge animate-pulse duration-76 z-10">
+    <div className="w-[200px] xl:w-[300px] absolute -right-16 lg:-bottom-[120px]  mix-blend-color-dodge animate-pulse duration-76 z-10">
       <Image
         src={"/circles.png"}
         width={260}
--- components/ParticlesContainer.js
+++ components/ParticlesContainer.js
@@ -37,7 +37,7 @@ const ParticlesContainer = () => {
           },
           modes: {
             push: {
-              quantity: 90,
+              quantity: 120,
             },
             repulse: {
               distance: 200,
